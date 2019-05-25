474733c383c904c68e932881b7051c5e243df74a
johao70 <perlazajohao@gmail.com>
2019-05-25 18:20:15 -0500

    novedades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent,
     HomeComponent
   ],
   imports: [
