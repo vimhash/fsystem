00d60b2e582ac8ec2f4f2c95493d7283c97bb452
johao70 <perlazajohao@gmail.com>
2019-05-25 20:06:03 -0500

    deudas page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { DeudasComponent } from "./deudas/deudas.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'deudas', component: DeudasComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
